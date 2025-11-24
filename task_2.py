0055ba8af58930abf6a15b5d9958b0666d9ec98d
Natalya <nidnatasha@gmail.com>
2025-11-24 09:33:11 +0000

    Implemented palindrome checking

--- task_2.py
+++ task_2.py
@@ -3,6 +3,14 @@ from collections import deque
 
 def palindrome(str_rts):
     double_queue = deque()
-
     for i in str_rts:
         double_queue.append(i)
+
+    while len(double_queue) > 1:
+        if double_queue.pop() != double_queue.popleft():
+            return False
+    return True
+
+
+print(palindrome("akzka"))
+print(palindrome("kazka"))
